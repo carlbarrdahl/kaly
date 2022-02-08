ece6de21a2dbf9be4aa7b3964e2cd0b50718fabd
Carl Barrdahl <carlbarrdahl@gmail.com>
2022-02-08 11:01:56 +0100

    Fix deploy

--- utils/timeslot.ts
+++ utils/timeslot.ts
@@ -1,28 +1,30 @@
-import {
-  isBefore,
-  setHours,
-  setMinutes,
-  setSeconds,
-  addMinutes,
-  setMilliseconds,
-} from "date-fns";
+// import {
+//   isBefore,
+//   setHours,
+//   setMinutes,
+//   setSeconds,
+//   addMinutes,
+//   setMilliseconds,
+// } from "date-fns";
 
-const setTime = (x, h = 0, m = 0, s = 0, ms = 0) =>
-  setHours(setMinutes(setSeconds(setMilliseconds(x, ms), s), m), h);
+// const setTime = (x, h = 0, m = 0, s = 0, ms = 0) =>
+//   setHours(setMinutes(setSeconds(setMilliseconds(x, ms), s), m), h);
 
-// Provide an array of availability with slot duration
+// // Provide an array of availability with slot duration
 
-export function createTimeslot(start, end, duration, availability) {
-  const timeslots = availability.rules.map((rule) => {});
-  const from = setTime(new Date(), 9);
-  const to = setTime(new Date(end), 17);
-  const step = (x) => addMinutes(x, 30);
+// export function createTimeslot(start, end, duration, availability) {
+//   const timeslots = availability.rules.map((rule) => {});
+//   const from = setTime(new Date(), 9);
+//   const to = setTime(new Date(end), 17);
+//   const step = (x) => addMinutes(x, 30);
 
-  const blocks = [];
-  let cursor = from;
+//   const blocks = [];
+//   let cursor = from;
 
-  while (isBefore(cursor, to)) {
-    blocks.push(cursor);
-    cursor = step(cursor);
-  }
-}
+//   while (isBefore(cursor, to)) {
+//     blocks.push(cursor);
+//     cursor = step(cursor);
+//   }
+// }
+
+export {};
