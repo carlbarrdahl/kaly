327ea72281080aba84ef9687534c57f18c89457a
Carl Barrdahl <carlbarrdahl@gmail.com>
2022-02-27 17:52:31 +0100

    Clean up Calendar

--- components/features/calendar/Calendar.tsx
+++ components/features/calendar/Calendar.tsx
@@ -68,47 +68,6 @@ const Calendar = ({
       // eventResize={handleEventResize}
       events={events}
       eventColor={theme.colors.blue["500"]}
-      //   events={[
-      //     {
-      //       id: "kjzl6cwe1jw148h72qxs6l5ljjr489hs5px4vjpcu6b3pmkti57parj8o84h61h",
-      //       end: "2022-02-08T18:00:00.000Z",
-      //       url: "",
-      //       start: new Date(),
-      //       title: "Yoga",
-      //       allDay: false,
-      //       attendees: [],
-      //       organizer:
-      //         "did:3:kjzl6cwe1jw1486v4vaq4amvob4cy7ix3hp7q9hfkkq1at57kdc2ivnw0cz8sja",
-      //       description: "yoga session",
-      //       color: theme.colors.purple["400"],
-      //       rrule: {
-      //         // rrule strings also work
-      //         freq: "daily",
-      //         interval: 1,
-      //         byweekday: ["mo", "we", "fr"],
-      //         dtstart: "2012-02-01T10:30:00", // will also accept '20120201T103000'
-      //         // until: "2012-06-01", // will also accept '20120201'
-      //       },
-      //     },
-
-      //     {
-      //       id: "kjzl6cwe1jw147x36zvr8unoa7edzh3opguckrnvwnce79b0lkwxbr92gdoc6yi",
-      //       end: "2022-02-09T10:00:00.000Z",
-      //       url: "",
-      //       start: "2022-02-09T09:00:00.000Z",
-      //       title: "60 min feedback",
-      //       allDay: false,
-      //       attendees: ["0x6A4E948a81E20802C91bB48D825A741a6F1E9062"],
-      //       organizer:
-      //         "did:3:kjzl6cwe1jw1486v4vaq4amvob4cy7ix3hp7q9hfkkq1at57kdc2ivnw0cz8sja",
-      //       description:
-      //         "# Feedback session\n\nMore info: https://github.com/carlbarrdahl/kaly\n\n## What is it?\nA decentralized calendar and scheduling app built with Ceramic.\n\n## How does it work?\n1. Sign in with Metamask\n2. Create an event\n3. Share link\n\n## Why?\n- DAOs can pull in events from their members\n- Composable calendars\n- For fun!",
-      //     },
-      //   ]}
-      //   events={{
-      //     url: "./ical.ics",
-      //     format: "ics",
-      //   }}
     />
   );
 };
